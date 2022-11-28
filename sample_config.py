8013c286e167627009108fbcae5f51301697a723
mlbtvps <119069067+mlbtvps@users.noreply.github.com>
2022-11-28 07:24:30 +0630

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -25,11 +25,11 @@
 class Config(object):
 
     # get a token from @BotFather
-    BOT_TOKEN = ""
+    BOT_TOKEN = "5705156853:AAETn1HpOHb0POCFGqUCwGOLyVHVN0qR_rM"
 
     # Get these values from my.telegram.org
-    API_ID = 1234567
-    API_HASH = ""
+    API_ID = 17894641
+    API_HASH = "4e5b39e5c7c6066e5144dfc50cf466cf"
 
     # No need to change
     DOWNLOAD_LOCATION = "./DOWNLOADS"
@@ -40,11 +40,11 @@ class Config(object):
     PROCESS_MAX_TIMEOUT = 3700
 
     # TG Ids
-    LOG_CHANNEL = -1007119867241
-    OWNER_ID = 1288398723
+    LOG_CHANNEL = -1001705637043
+    OWNER_ID = 5468192421
 
     # bot username without @
-    BOT_USERNAME = "AdvanceUrlUploaderBot"
+    BOT_USERNAME = "NyiAnydlBot"
 
     # auth users
     AUTH_USERS = [OWNER_ID]
